a659039af01322991c578e750be7489a6ca018e6
Mahran Elmasri <mahranelmasri@Mahrans-MBP.local>
2019-06-11 12:47:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { recipes } from "./tempList";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    recipes: recipes,
+    url:
+      "https://www.food2fork.com/api/search?key=2e0f86cfc76d54ebe381c1cd235a5f6b",
+    base_url:
+      "https://www.food2fork.com/api/search?key=2e0f86cfc76d54ebe381c1cd235a5f6b",
+    details_id: 35382,
+    pageIndex: 1,
+    search: "",
+    query: "&q=",
+    error: ""
+  };
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      if (jsonData.recipes.length === 0) {
+        this.setState(() => {
+          return { error: "Sorry, but your search not return any results" };
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayItems(index) {
+    switch (index) {
+      case 1:
+        return (
+          <RecipeList
+            recipes={this.state.recipes}
+            handleDetails={this.handleDetails}
+            value={this.state.search}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            error={this.state.error}
+          />
+        );
+      case 0:
+        return (
+          <RecipeDetails
+            id={this.state.details_id}
+            handleIndex={this.handleIndex}
+          />
+        );
+      default:
+    }
+  }
+  handleIndex = index => {
+    this.setState({
+      pageIndex: index
+    });
+  };
+  handleDetails = (index, id) => {
+    this.setState({
+      pageIndex: index,
+      details_id: id
+    });
+  };
+  handleChange = e => {
+    this.setState(
+      {
+        search: e.target.value
+      },
+      () => {
+        console.log(this.state.search);
+      }
+    );
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      () => {
+        return { url: `${base_url}${query}${search}`, search: "" };
+      },
+      () => {
+        this.getRecipes();
+      }
+    );
+  };
+  render() {
+    return <div className="App">{this.displayItems(this.state.pageIndex)}</div>;
+  }
 }
 
 export default App;
---
+++ src/components/RecipeList.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Recipe from "./Recipe";
+import RecipeSearch from "./RecipeSearch";
+
+export default class RecipeList extends Component {
+  render() {
+    const {
+      recipes,
+      handleDetails,
+      value,
+      handleSubmit,
+      handleChange,
+      error
+    } = this.props;
+    // console.log(recipes);
+
+    return (
+      <React.Fragment>
+        <RecipeSearch
+          value={value}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+        />
+        <div className="container my-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/*end of title */}
+          <div className="row">
+            {error ? (
+              <h1 className="text-danger text-center mx-auto">{error}</h1>
+            ) : (
+              recipes.map(recipe => {
+                return (
+                  <Recipe
+                    key={recipe.recipe_id}
+                    recipe={recipe}
+                    handleDetails={handleDetails}
+                  />
+                );
+              })
+            )}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
